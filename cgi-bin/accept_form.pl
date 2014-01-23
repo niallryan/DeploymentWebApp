a3d21fd2b77a826671efe8daaf1dcc15e1121250
Niall Ryan <ryann4@tcd.ie>
2014-01-23 22:29:21 +0000

    Change localhost to production server in accept_form.pl

--- cgi-bin/accept_form.pl
+++ cgi-bin/accept_form.pl
@@ -21,16 +21,16 @@ sub insertDB {
 my $name = shift;
 my $address =shift;
 
-my $dbhost='127.0.0.1'; my $dbport=3306;
+my $dbhost='54.194.154.110'; my $dbport=3306;
 my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
-$dbh = DBI->connect($dsn, 'dbroot', 'dbpassword'
+$dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
                 ) || die "Could not connect to database: $DBI::errstr";
 $sth = $dbh->prepare("insert into custdetails(name,address) values(?,?)");
 $sth->execute($name,$address);
 }
 
 sub showDB {
-my $dbhost='127.0.0.1'; my $dbport=3306;
+my $dbhost='54.194.154.110'; my $dbport=3306;
 my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
 $dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
                 ) || die "Could not connect to database: $DBI::errstr";
