ca5b83b88a7758ced0650c9d50491c568c679178
Niall Ryan <ryann4@tcd.ie>
2014-01-23 22:37:28 +0000

    Tweaking perl script again

--- cgi-bin/accept_form.pl
+++ cgi-bin/accept_form.pl
@@ -21,7 +21,7 @@ sub insertDB {
 my $name = shift;
 my $address =shift;
 
-my $dbhost='54.194.154.110'; my $dbport=3306;
+my $dbhost='127.0.0.1'; my $dbport=3306;
 my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
 $dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
                 ) || die "Could not connect to database: $DBI::errstr";
@@ -30,7 +30,7 @@ $sth->execute($name,$address);
 }
 
 sub showDB {
-my $dbhost='54.194.154.110'; my $dbport=3306;
+my $dbhost='127.0.0.1'; my $dbport=3306;
 my $dsn="DBI:mysql:dbtest;host=$dbhost;port=$dbport";
 $dbh = DBI->connect($dsn, 'dbtestuser', 'dbpassword'
                 ) || die "Could not connect to database: $DBI::errstr";
